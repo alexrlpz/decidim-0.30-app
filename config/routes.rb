a1d624e3b4aba7a04fe16b499a8fae0fb3b86baa
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-05-29 09:52:45 +0300

    Init app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount Decidim::Core::Engine => '/'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  #
+  mount Decidim::FileAuthorizationHandler::AdminEngine => '/admin'
+end
