290a17fdbc070dfd1d906a8395e2efab70e8e87e
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-06-29 20:28:33 +0300

    Disable file authorization handler

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   #
-  mount Decidim::FileAuthorizationHandler::AdminEngine => '/admin'
+  #mount Decidim::FileAuthorizationHandler::AdminEngine => '/admin'
 end
